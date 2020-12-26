0997318347772cd0257b69a505858c10dc0c0025
Omnia254 <76522001+Omnia254@users.noreply.github.com>
2020-12-26 16:14:10 +0200

    Create showcourses.java

---
+++ src/StudentModule/showcourses.java
@@ -0,0 +1,48 @@
+package StudentModule;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+
+public class showcourses {
+   static Connection c;
+    static Statement ss;
+    static String query;
+      static ResultSet rs;
+      public void showcourse()
+      {
+     conn c1=new conn();
+     
+          try {
+              ArrayList<elem> list=new ArrayList<elem>();
+           // ArraytList<elem> list=new ArraytList<elem>();
+            c=c1.connect();
+            ss=c.createStatement(); 
+            query="select * from courses";
+            rs=ss.executeQuery(query);
+           
+             System.out.println("CourseID\t\tCourseName\t\tParentCourse");
+            while(rs.next())
+            {    
+               elem a= new elem(rs.getString("courseID"),rs.getString("courseName"),rs.getString("parentCourse"));
+                list.add(a); 
+            }
+         for(int i=0;i<list.size();i++)
+         {
+          System.out.print(list.get(i).id);
+           System.out.print("\t\t\t");
+           System.out.print(list.get(i).Namecourse);
+            System.out.print("\t\t\t");
+            System.out.print(list.get(i).parentcourse);
+             System.out.println();
+         }
+                 
+        } catch (SQLException ex){
+           System.out.println( ex);
+        }  
+          
+}
+}
