cb5dcc9b5ba52cdd4a2d1123168b09ce2e52e591
Rewaa612002 <76520989+Rewaa612002@users.noreply.github.com>
2020-12-26 16:12:08 +0200

    Add files via upload

---
+++ src/CourseModule/DBconnect.java
@@ -0,0 +1,10 @@
+package MainDriver;
+import java.sql.*;
+public class DBconnect {
+    private final String connectionUrl = "jdbc:sqlserver://localhost:1433;user=project;password=1234;"
+                + "databaseName=CourseManagementSystem;";
+    public Connection connect() throws SQLException {
+ 
+        return DriverManager.getConnection(connectionUrl);
+    }
+}
