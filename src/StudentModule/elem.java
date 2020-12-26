ed1cc71648159010f150cf9ace7ef19e81a0c7c7
Omnia254 <76522001+Omnia254@users.noreply.github.com>
2020-12-26 16:06:56 +0200

    Create elem.java

---
+++ src/StudentModule/elem.java
@@ -0,0 +1,17 @@
+package StudentModule;
+
+public class elem {
+   String id;
+    String Namecourse;
+ String parentcourse;
+     // public elem(){}
+    public  elem(String x,String n,String p)
+    {
+     this.id=x;
+     this.Namecourse=n;
+     this.parentcourse=p;
+    }
+
+  
+    
+}
