c928b776ffe17cacfde8346f942006541f96c6ba
Rewaa612002 <76520989+Rewaa612002@users.noreply.github.com>
2020-12-27 02:11:52 +0200

    Update DBconnect.java

--- src/MainDriver/DBconnect.java
+++ src/MainDriver/DBconnect.java
@@ -1,20 +1,15 @@
-/*Source codes of DBconnect.java*/
 package MainDriver;
 
 import java.sql.*;
 
 public class DBconnect {
 
-    public static Connection connect() throws ClassNotFoundException {
-        Connection con = null;
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String connectionURL = "jdbc:sqlserver://localhost:1433;databaseName=CourseManagementSystem;user=admin;password=admin";
-        try {
-            con = DriverManager.getConnection(connectionURL);
-            //System.out.println("Connection stablished successfully.");
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return con;
+    private final String connectionUrl = "jdbc:sqlserver://localhost:1433;user=project;password=1234;"
+            + "databaseName=CourseManagementSystem;";
+
+    public Connection connect() throws SQLException {
+
+        return DriverManager.getConnection(connectionUrl);
     }
+
 }
